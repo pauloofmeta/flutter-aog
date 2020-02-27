96fa67e096bfc7d8358cb0ee4bc86e9e6654a4ce
paulo.henrique <paulo.henrique@softplan.com.br>
2020-02-27 10:23:06 -0300

    Change title

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Alcool ou Gasolina App',
+      title: 'Alcool ou Gasolina',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
